b8bec5b3a5f7a88327a126c19d86edead84375f4
“Prachi-Lal <plalindia01@gmail.com>
2026-02-11 01:39:12 +0530

    cors fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ app = FastAPI(title="Contact API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "https://quickbuildr-assessment.vercel.app"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
